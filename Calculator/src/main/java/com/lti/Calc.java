261fa284ab878fbf262c3422057e5b2f141dc6f4
Ruthvik Roy <ruthvikroy.chinna@gmail.com>
2020-12-21 15:15:29 +0530

    added division method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -14,10 +14,15 @@ public class Calc {
 		return num1 * num2;
 	}
 	
+	public static int div(int num1, int num2) {
+		return num1 / num2;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Sum is : "+add(10, 20));
 		System.out.println("Difference is : "+subs(40, 20));
 		System.out.println("Product is : "+prod(10, 20));
+		System.out.println("Division is : "+div(200, 20));
 	}
 
 }
