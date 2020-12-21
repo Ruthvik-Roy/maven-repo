b1e5e5d4ffde453c2cf04909f9f3f7885b36e407
Ruthvik Roy <ruthvikroy.chinna@gmail.com>
2020-12-21 15:09:50 +0530

    added product method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -10,9 +10,14 @@ public class Calc {
 		return num1 - num2;
 	}
 	
+	public static int prod(int num1, int num2) {
+		return num1 * num2;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Sum is : "+add(10, 20));
-		System.out.println("Difference is : "+subs(10, 20));
+		System.out.println("Difference is : "+subs(40, 20));
+		System.out.println("Product is : "+prod(10, 20));
 	}
 
 }
